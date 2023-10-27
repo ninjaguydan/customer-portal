147488e9b65f03a24b6c7acf421cb2908ed036bb
ninjaguydan <dthompson0190@gmail.com>
2023-10-27 16:45:44 -0500

    added typing for images; added gh-pages

---
+++ src/custom.d.ts
@@ -0,0 +1,10 @@
+declare module "*.svg" {
+  // const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  // export default content;
+  const value: any;
+  export = value;
+}
+declare module "*.png" {
+  const value: any;
+  export = value;
+}
